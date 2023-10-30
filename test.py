a4495dda259206ecccc307a05c6a3976545186a0
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-10-30 19:55:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,52 +5,60 @@ Created on Mon Oct 30 19:01:12 2023
 @author: Tobias Kurz
 """
 
-def up(duration):
-    # commands
-    
-    return
-
-def down(duration):
-    # commands
-    
-    return
-
-def left(duration):
-    # commands
-    
-    return
-
-def right(duration):
-    # commands
-    
-    return
-
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
 # Define the GPIO pins for each relay
-relay_pins = [17, 18, 22, 27]  # Example GPIO pins, adjust as per your wiring
-
-
-
-
+relay_pins = [17, 18, 22, 27]
 
 # Set the relay pins as output
 for pin in relay_pins:
     GPIO.setup(pin, GPIO.OUT)
 
-try:
-   
-    # Turn on each relay for 1 second and then turn it off
-    for pin in relay_pins:
-        GPIO.output(pin, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(1)
-except:
-    pass
+def up(duration=1, pins=relay_pins):
+    pin = pins[1]
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(pin, GPIO.LOW) 
+    GPIO.cleanup()
+    return()
 
-GPIO.cleanup()
+def down(duration=1, pins=relay_pins):
+    pin = pins[2]
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(pin, GPIO.LOW) 
+    GPIO.cleanup()
+    return()
+
+def left(duration=1, pins=relay_pins):
+    pin = pins[0]
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
+    return()
+
+def right(duration=1, pins=relay_pins):
+    pin = pins[3]
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
+    return()
+
+
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+left()
+right()
+up()
+down()
+
+
+
+
+
+
