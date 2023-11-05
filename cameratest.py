bc708694f34275a405104556a4252102660f1aa5
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-11-05 19:46:10 +0100

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -10,9 +10,4 @@ import time
 
 picam2 = Picamera2()
 
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+picam2.start_and_capture_file("test.jpg")
