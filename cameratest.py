77a4cda6922d391cab017765ae503d040ad0c4fb
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-11-05 21:32:16 +0100

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -5,9 +5,31 @@ Created on Sun Nov  5 19:41:05 2023
 @author: Tobias Kurz
 """
 
-from picamera2 import Picamera2, Preview
+from picamera2 import PiCamera2, Preview, PiRGBArray
 import time
 
-picam2 = Picamera2()
+# import the necessary packages
 
-picam2.start_and_capture_file("test.jpg")
+
+import time
+import cv2
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera2()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
