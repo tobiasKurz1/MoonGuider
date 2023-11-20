37bc90785ad9b6dd003a50112b5f941e2ab88762
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-11-20 22:50:30 +0100

    better test

--- relay_test.py
+++ relay_test.py
@@ -55,58 +55,60 @@ def right(duration=1, pins=relay_pins):
 
 
 # Prompt for the number of iterations, frequency in Hz, and mode (Left, Up, Down, Right)
-iterations = int(input("Iterations: "))
-fz = 1 / float(input("Frequency in Hz: "))
-mode = input("Mode (RLDU): ")
+iterations = int(input("Iterations (press enter for general test): "))
 
-# Convert input to list of characters
-on = [char for char in mode]
-
-# Loop for the specified number of iterations
-for i in range(iterations):
-    print(i + 1)  # Print the current iteration number
-
-    # Activate the specified relays based on the mode
-    for i in range(len(on)):
-        if int(on[i]) == 1:
-            GPIO.output(relay_pins[i], GPIO.LOW)
-
-    # Pause for the specified time (frequency)
-    time.sleep(fz / 2)
-
-    # Deactivate the relays based on the mode
-    for i in range(len(on)):
-        if int(on[i]) == 1:
-            GPIO.output(relay_pins[i], GPIO.HIGH)
-
-    # Pause for the specified time (frequency)
-    time.sleep(fz / 2)
-
-
+if iterations:
+    fz = 1 / float(input("Frequency in Hz: "))
+    mode = input("Mode (RLDU): ")
     
-
-"""
-
-# Try every possible combination of activations. Press Enter in between.
-
-alles = ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
-print("LUDR")
-for test in alles:
-    print(test)
-    on = [char for char in test]
-    try:
-               
+    # Convert input to list of characters
+    on = [char for char in mode]
+    
+    # Loop for the specified number of iterations
+    for i in range(iterations):
+        print(i + 1)  # Print the current iteration number
+    
+        # Activate the specified relays based on the mode
         for i in range(len(on)):
             if int(on[i]) == 1:
-                GPIO.output(relay_pins[i], GPIO.LOW)   
-        input()
+                GPIO.output(relay_pins[i], GPIO.LOW)
+    
+        # Pause for the specified time (frequency)
+        time.sleep(fz / 2)
+    
+        # Deactivate the relays based on the mode
         for i in range(len(on)):
             if int(on[i]) == 1:
-                GPIO.output(relay_pins[i], GPIO.HIGH)       
-        input()
-    except:
-        break
-"""
+                GPIO.output(relay_pins[i], GPIO.HIGH)
+    
+        # Pause for the specified time (frequency)
+        time.sleep(fz / 2)
+    
+    
+        
+    
+else:
+
+    # Try every possible combination of activations. Press Enter in between.
+    
+    alles = ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
+    print("Testing every possible combination...")
+    for test in alles:
+        print(test)
+        on = [char for char in test]
+        try:
+                   
+            for i in range(len(on)):
+                if int(on[i]) == 1:
+                    GPIO.output(relay_pins[i], GPIO.LOW)   
+            input()
+            for i in range(len(on)):
+                if int(on[i]) == 1:
+                    GPIO.output(relay_pins[i], GPIO.HIGH)       
+            input()
+        except:
+            break
+
 
 GPIO.cleanup()
 
