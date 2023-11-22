b394ff0aa1c44c083e0292d07f83ade2180fa527
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-11-22 12:25:48 +0100

    Update movement_test.py

--- movement_test.py
+++ movement_test.py
@@ -105,7 +105,7 @@ while True:
         reference_x,
         reference_y,
         processed,
-        handover_value = f"Active relays: {guide.showactive()}    {i} / {step*5}",
+        handover_value = f"Active relays: {guide.showactive()}    {i-1} / {step*5}; rounds left:{rounds-1}",
         overlay = True,
         scale = 1        
         )
