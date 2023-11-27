02fbeae963219b1c0f07dd050798edaedb43f901
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2023-11-28 00:02:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,40 +2,43 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import time
 
 # Create figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
+x = []
+y1 = []
+y2 = []
 
-# Initialize communication with TMP102
 
 
 # This function is called periodically from FuncAnimation
-def animate(i, xs, ys):
-
-    # Read temperature (Celsius) from TMP102
-    temp_c = np.random.random()
-
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(temp_c)
-
-    # Limit x and y lists to 20 items
-    xs = xs[-200:]
-    ys = ys[-200:]
-
+def animate(x, y1, y2):
+    
+    buff = 100
+    
+    xs = x[-buff:]
+    y1s = y1[-buff:]
+    y2s = y2[-buff:]
+    
+    
     # Draw x and y lists
     ax.clear()
-    ax.plot(xs, ys)
+    ax.plot(xs, y1s, label="y1")
+    ax.plot(xs, y2s, label="y2")
 
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('TMP102 Temperature over Time')
-    plt.ylabel('Temperature (deg C)')
+    plt.title('TEst')
+    plt.ylabel('Values')
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=500)
+ani = animation.FuncAnimation(fig, animate, fargs=(x, y1, y2), interval=500)
 plt.show()
+
+while True:
+    x.append(time.ctime())
+    y1.append(np.random.random())
+    y2.append(np.random.random())
