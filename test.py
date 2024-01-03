133dff7e01d5a317ed702ae7e6bd310da56b441e
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2024-01-03 16:07:02 +0100

    button mode

--- test.py
+++ test.py
@@ -1,49 +1,25 @@
-import datetime as dt
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
-import time
+import RPi.GPIO as GPIO
 
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-x = []
-y1 = []
-y2 = []
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+button_pin = 17  # Change this to the actual GPIO pin you've connected the button to
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+def button_callback(channel):
+    print("Button pressed!")
 
+# Set up event detection
+GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
 
-# This function is called periodically from FuncAnimation
-def animate(i, x, y1, y2):
-    
-    buff = 100
-    
-    x = x[-buff:]
-    y1 = y1[-buff:]
-    y2 = y2[-buff:]
-    
-    
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(x, y1, label="y1")
-    ax.plot(x, y2, label="y2")
+try:
+    print("Press Ctrl+C to exit")
+    while True:
+        # Your main program can run here
+        pass
 
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('TEst')
-    plt.ylabel('Values')
-    plt.legend()
+except KeyboardInterrupt:
+    pass
 
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(x, y1, y2), interval=500)
-def update_data():
-    x.append(time.ctime())
-    y1.append(np.random.random())
-    y2.append(np.random.random())
-
-
-# Main loop to update data
-while True:
-    update_data()
-    plt.pause(1)  # Adjust the pause duration as needed
+finally:
+    # Clean up GPIO on exit
+    GPIO.cleanup()
