2583dca2a2e49e389600fc64f9011ae1741e520e
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2024-01-05 16:22:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ while True:
     if guide.button_is_pressed() and not None in (avrg_target_x, avrg_target_y):
         (reference_x, reference_y) = (avrg_target_x, avrg_target_y)
         press_counter += 1
-        if press_counter >= 3:
+        if press_counter >= 2:
             (reference_x, reference_y) = image_center            
     else:
         press_counter = 0
