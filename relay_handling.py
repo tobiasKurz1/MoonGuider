61f72fe8614cfd02df56b44a1c3a1db7ff7906fc
tobiasKurz1 <104297637+tobiasKurz1@users.noreply.github.com>
2024-01-09 21:46:22 +0100

    testing pulse activation

--- relay_handling.py
+++ relay_handling.py
@@ -172,6 +172,10 @@ class guide:
             GPIO.output(self.relay_pins[3], GPIO.HIGH)
             self.active[3] = False
         
+        
+        ### TEST ###
+        time.sleep(0.1)
+        for pin in self.relay_pins: GPIO.output(pin, GPIO.HIGH)
         return
 
     
